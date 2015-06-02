98450bc87315c3ca3a78ae6ba38a853972b3d188
droglenc <dogle@northland.edu>
2015-06-02 00:42:58 -0500

    moved fsaNews() into FSAUtils file.

--- R/FSAUtils.R
+++ R/FSAUtils.R
@@ -257,6 +257,39 @@ fishR <- function(where=c("home","IFAR","general","books","AIFFD","posts","news"
 }
 
 
+#' @title Read news and changes for the 'FSA' package.
+#'
+#' @description Opens up the \href{https://github.com/droglenc/FSA/blob/master/NEWS.md}{News.md GitHub file} for the \sQuote{FSA} package in an external browser.
+#' 
+#' @aliases fsaNews FSANews
+#' 
+#' @return None.
+#' 
+#' @author Derek H. Ogle, \email{dogle@@northland.edu}
+#' 
+#' @keywords manip
+#' 
+#' @examples
+#' ## ONLY RUN IN INTERACTIVE MODE
+#' if (interactive()) {
+#' 
+#' fsaNews()
+#' FSANews()
+#'
+#'}  ## END IF INTERACTIVE MODE
+#' 
+#' @rdname fsaNews
+#' @export
+fsaNews <- function () {
+  browseURL("https://github.com/droglenc/FSA/blob/master/NEWS.md")
+}
+
+#' @rdname fsaNews
+#' @export
+FSANews <- function () {
+  fsaNews()
+}
+
 
 #' Shows rows from the head and tail of a data frame or matrix.
 #'
