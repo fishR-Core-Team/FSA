5d9e724839a69506d569367fc72f3218d2b04779
Derek Ogle <dogle@northland.edu>
2016-11-20 17:39:18 -0600

    Converted AgeComparisons Tests

--- tests/testthat/test_AgeComparisons_VALIDATE.R
+++ tests/testthat/test_AgeComparisons_VALIDATE.R
@@ -1,41 +1,4 @@
-context("Age Comparisons (Precision and Bias)")
-
-test_that("ageBias() errors and warnings",{
-  data(WhitefishLC)
-  ## Two variables on LHS
-  expect_error(ageBias(otolithC+scaleC~finrayC,data=WhitefishLC),"more than one variable on the LHS")
-  ## Two variables on RHS
-  expect_error(ageBias(otolithC~scaleC+finrayC,data=WhitefishLC),"must have only one RHS")
-  ## Bad choices for what= in plot and summary
-  suppressWarnings(ab1 <- ageBias(scaleC~otolithC,data=WhitefishLC))
-  expect_error(summary(ab1,what="derek"),"should be one of")
-  expect_error(plot(ab1,what="derek"),"should be one of")
-  ## Bad choice for cont.corr
-  expect_error(summary(ab1,what="McNemar",cont.corr="derek"),"should be one of")
-  ## Test that messages are printed
-  expect_message(summary(ab1),"Sample size in the age agreement table")
-  expect_message(summary(ab1),"Summary of scaleC by otolithC")
-  expect_message(summary(ab1),"Summary of scaleC-otolithC by otolithC")
-  expect_message(summary(ab1),"square")
-  expect_message(summary(ab1,flip.table=TRUE),"flipped")
-  expect_message(summary(ab1),"Age agreement table symmetry test results")
-})
-
-test_that("agePrecision() errors and warnings",{
-  data(WhitefishLC)
-  ap1 <- agePrecision(~otolithC+scaleC,data=WhitefishLC)
-  ## Bad choices for what= in summary
-  expect_error(summary(ap1,what="agreement"),"should be one of")
-  ## Test that messages are printed
-  expect_message(summary(ap1),"Precision summary statistics")
-  expect_message(summary(ap1),"Percentage of fish by absolute differences in ages")
-  expect_message(summary(ap1),"Percentage of fish by differences in ages")
-  expect_message(summary(ap1),"Intermediate calculations for each individual")
-  ## Test that trunc.diff is OK
-  expect_error(summary(ap1,what="absolute",trunc.diff=0),"must be positive")
-  expect_error(summary(ap1,what="absolute",trunc.diff=-2),"must be positive")
-})
-
+context("Age Comparisons (Precision and Bias) VALIDATE")
 
 test_that("ageBias() symmetry tests match the results in Evans and Hoenig (2008)",{
   ######## Create Evans & Hoenig (2008) X, Y, and Z matrices and check
