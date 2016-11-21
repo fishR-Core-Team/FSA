bfccc7bd7ec13b1a5e664e6fc90e86b305071e05
Derek Ogle <dogle@northland.edu>
2016-11-20 21:32:44 -0600

    Converted dunnTest() tests

---
+++ tests/testthat/test_dunnTest_MSG.R
@@ -0,0 +1,13 @@
+context("dunnTest() MESSAGES")
+source("EXS_dunnTest.R")
+
+test_that("dunnTest() error and warning messages",{
+  expect_error(dunnTest(ponds$pond,ponds$pH),"to a factor")
+  expect_warning(dunnTest(ponds$pH,ponds$pond),"to a factor")
+  expect_warning(dunnTest(ponds$pH,ponds$cpond),"to a factor")
+  expect_error(dunnTest(pond~pH,data=ponds),"must be a factor")
+  expect_warning(dunnTest(pH~pond,data=ponds),"to a factor")
+  expect_warning(dunnTest(pH~cpond,data=ponds),"to a factor")
+  expect_error(dunnTest(pH~pond+cpond,data=ponds),"only one RHS variable")
+  expect_error(dunnTest(pH+cpond~pond,data=ponds))
+})
--- tests/testthat/test_dunnTest_VALIDATE.R
+++ tests/testthat/test_dunnTest_VALIDATE.R
@@ -1,28 +1,5 @@
-context("dunnTest() function Tests")
-## pH in four ponds data from Zar (2010)
-ponds <- data.frame(pond=rep(1:4,each=8),
-                     pH=c(7.68,7.69,7.70,7.70,7.72,7.73,7.73,7.76,
-                          7.71,7.73,7.74,7.74,7.78,7.78,7.80,7.81,
-                          7.74,7.75,7.77,7.78,7.80,7.81,7.84,NA,
-                          7.71,7.71,7.74,7.79,7.81,7.85,7.87,7.91))
-ponds$cpond <- paste0("pond_",ponds$pond)
-ponds$fpond <- as.factor(ponds$pond)
-ponds <- ponds[complete.cases(ponds),]
-
-## p-value results from UniStat (http://www.unistat.com/guide/nonparametric-tests-kruskal-wallis-one-way-anova/)
-unistat <- data.frame(Comparison=c("2-1","3-1","3-2","4-1","4-2","4-3"),
-                      P.adj=c(0.1956,0.0191,1,0.0166,1,1))
-
-test_that("dunnTest() error and warning messages",{
-  expect_error(dunnTest(ponds$pond,ponds$pH),"to a factor")
-  expect_warning(dunnTest(ponds$pH,ponds$pond),"to a factor")
-  expect_warning(dunnTest(ponds$pH,ponds$cpond),"to a factor")
-  expect_error(dunnTest(pond~pH,data=ponds),"must be a factor")
-  expect_warning(dunnTest(pH~pond,data=ponds),"to a factor")
-  expect_warning(dunnTest(pH~cpond,data=ponds),"to a factor")
-  expect_error(dunnTest(pH~pond+cpond,data=ponds),"only one RHS variable")
-  expect_error(dunnTest(pH+cpond~pond,data=ponds))
-})
+context("dunnTest() VALIDATE")
+source("EXS_dunnTest.R")
 
 test_that("dunnTest matches UnitStat results",{
   tmp <- dunnTest(pH~fpond,data=ponds,method="bonferroni")
