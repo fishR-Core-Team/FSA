99ba14fed056e1a38cf05ea9fa6b05437aa1d703
Derek Ogle <dogle@northland.edu>
2016-11-21 09:35:25 -0600

    Converted ksTest() Tests

--- tests/testthat/test_ksTest_MSG.R
+++ tests/testthat/test_ksTest_MSG.R
@@ -1,4 +1,5 @@
-context("ksTest() tests")
+context("ksTest() MESSAGES")
+
 test_that("ksTest() messages",{
   ## Get data, make two more factor variables
   data(iris)
@@ -16,24 +17,4 @@ test_that("ksTest() messages",{
   expect_error(ksTest(Sepal.Length~Species+fact1+fact2,data=iris),"must contain 1 response")
   ## Problems with number of levels in factor variable
   expect_error(ksTest(Sepal.Length~Species,data=iris),"RHS variable has two levels")
-})
-
-test_that("Does ksTest() match ks.test()",{
-  x <- rnorm(50)
-  y <- runif(30)
-  df <- data.frame(dat=c(x,y),grp=rep(c("X","Y"),c(50,30)))
-  
-  ## one-sample (from ks.test) still works
-  tmp1 <- ksTest(x+2, "pgamma", 3, 2)
-  tmp2 <- ks.test(x+2, "pgamma", 3, 2)
-  expect_equal(tmp1$statistic,tmp2$statistic)
-  expect_equal(tmp1$p.value,tmp2$p.value)
-  expect_is(tmp1,"htest")
-  
-  ## first two-sample example in ?ks.test
-  tmp1 <- ksTest(x,y)
-  tmp2 <- ks.test(x,y)
-  tmp3 <- ksTest(dat~grp,data=df)
-  expect_equal(tmp1,tmp2)
-  expect_equal(tmp3,tmp2)
 })
