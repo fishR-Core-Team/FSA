8d805fc8049be87c06d114ad8ef6321cb5f8e16b
Derek Ogle <dogle@northland.edu>
2018-03-31 16:37:51 -0500

    Added test for Dunns test #40

--- tests/testthat/test_dunnTest_OUT.R
+++ tests/testthat/test_dunnTest_OUT.R
@@ -4,7 +4,8 @@ source("EXS_dunnTest.R")
 test_that("dunnTest() output",{
   if (require(dunn.test)) {
     ## Loop through all methods in p.adjustment.methods
-    lbls <- c("No Adjustment","Bonferroni","Sidak","Holm","Holm-Sidak","Hochberg","Benjamini-Hochberg","Benjamini-Yekuteili")
+    lbls <- c("No Adjustment","Bonferroni","Sidak","Holm","Holm-Sidak",
+              "Hochberg","Benjamini-Hochberg","Benjamini-Yekuteili")
     meths <- dunn.test::p.adjustment.methods
     for (i in seq_along(meths)) {  ## For two-sided cases
       tmp <- dunnTest(pH~fpond,data=ponds,method=meths[i],two.sided=TRUE)
@@ -23,4 +24,16 @@ test_that("dunnTest() output",{
       expect_equal(names(tmp$res),c("Comparison","Z","P.unadj","P.adj"))
     }
   }
+})
+
+test_that("Check dunnTest() output labels",{
+  ## This issue stems from a user complaint that labels for factors that
+  ## don't exist (as would happen from using subset() or filter() screw
+  ## up the results.
+  ponds3 <- subset(ponds,pond!=3)
+  tmp2 <- dunnTest(pH~fpond,data=subset(ponds,pond!=3))
+  expect_false(any(grepl("3",levels(tmp2$res$Comparison))))
+  expect_true(any(grepl("1",levels(tmp2$res$Comparison))))
+  expect_true(any(grepl("2",levels(tmp2$res$Comparison))))
+  expect_true(any(grepl("4",levels(tmp2$res$Comparison))))
 })
